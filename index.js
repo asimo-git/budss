624524d3ae05eceb9b586c6bbe96ec83ab4dcf15
Asimo <rvolenc@gmail.com>
2024-12-18 16:57:35 +0400

    feat: implement cookie modal window

--- index.js
+++ index.js
@@ -13,3 +13,31 @@ document.addEventListener("DOMContentLoaded", () => {
   burgerBtn.addEventListener("click", toggleMenu);
   closeBtn.addEventListener("click", toggleMenu);
 });
+
+function getCookie(name) {
+  console.log(document.cookie);
+  let matches = document.cookie.match(
+    new RegExp(
+      "(?:^|; )" +
+        name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, "\\$1") +
+        "=([^;]*)"
+    )
+  );
+  return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+let cookiecook = getCookie("cookiecook"),
+  cookiModal = document.querySelector(".cookie-modal");
+
+if (cookiecook != "no") {
+  cookiModal.classList.add("cookie-modal_active");
+
+  document
+    .querySelector(".cookie-modal__accept-btn")
+    .addEventListener("click", function () {
+      cookiModal.classList.remove("cookie-modal_active");
+      let date = new Date();
+      date.setDate(date.getDate() + 30);
+      document.cookie = "cookiecook=no; path=/; expires=" + date.toUTCString();
+    });
+}
